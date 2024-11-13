a4020cbdc5dddc7541b3fb5cea15a3c12dbc0272
jonnybee <jonny.bekkum@gmail.com>
2024-11-13 09:02:49 +0100

    Added bencher for EF8 vs EF9

--- EFCoreBencher/Program.cs
+++ EFCoreBencher/Program.cs
@@ -1,22 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using BenchmarkDotNet.Running;
-using EFCoreBenchmark;
-using System;
 
-namespace EFCoreBenchMark
+
+namespace EFCoreBenchMark;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            var summary = BenchmarkRunner.Run(typeof(EFCoreBencher).Assembly);
-            Console.WriteLine(summary);
-        }
+        BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
     }
 }
-
-
-
-
-
-
